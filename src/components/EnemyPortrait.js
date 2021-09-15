5ac5857b631ca6c27c7bf85e79a9e2c7843af95f
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-09-15 11:19:22 -0600

    add images& portraits

---
+++ src/components/EnemyPortrait.js
@@ -0,0 +1,11 @@
+import React from 'react'
+let enemyIMG = ""
+
+function EnemyPortrait( {name} ) {
+   return (
+      <h2>name.props</h2>
+      <img src = {enemyIMG} alt ='ENEMY'/>
+   );
+}
+
+export default EnemyPortrait
---
+++ src/components/PlayerPortrait.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import ImgSrc from ../images/duck.jpg
+let playerIMG = 
+
+function PlayerPortrait(props) {
+return( 
+
+<div>
+            <h2>props.name</h2>
+    <img src={playerIMG} alt="PLAYER"/>
+        {/* <h3>props.class</h3> */} 
+
+</div>
+);
+}
+
+
+export default PlayerPortrait
