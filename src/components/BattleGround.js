5d0e86313c9139cb39bff1188b62eec4357e81f8
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-09-17 06:17:15 -0600

    updated battleground

---
+++ src/components/BattleGround.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PlayerPortrait from ./PlayerPortrait
+import EnemyPortrait from ./ EnemyPortrait
+import Hand from ./Hand
+import hpBar from ./hpBar
+
+
+
+fucntion BattleGround(){
+  return(
+    <div className='Battleground'>  
+      </hpBar>
+      <div>
+      </PlayerPortrait>
+      </div>
+      <div>
+       </EnemyPortrait>
+    </div> 
+    <span>
+        </Hand>
+    </span>  
+
+    </div>
+  )
+
+}
+
+export default BattleGround;
